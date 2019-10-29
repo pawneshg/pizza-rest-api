bba99365bbcdac5e23337fa2889c87c6d4def7ab
Pawnesh Gautam <pawneshg@gmail.com>
2019-10-29 14:59:18 +0530

    Update settings.py

--- pizza_project/settings.py
+++ pizza_project/settings.py
@@ -138,5 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = 'static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 #AUTH_USER_MODEL = 'pizza_api.UserProfile'
